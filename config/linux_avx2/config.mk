06144215ae0caaf14e60c85bff379f8a9b4e26f3
Jaden Mu <68563142+NinjadenMu@users.noreply.github.com>
2026-07-30 00:29:29 -0400

    avx2 defined by avx2 extension, not x86-64

--- config/linux_avx2/config.mk
+++ config/linux_avx2/config.mk
@@ -1,5 +1,5 @@
 CONFIG_TARGET_TRIPLE := x86_64-unknown-linux-gnu
 CONFIG_TARGET_OS := Linux
 
-CONFIG_DISPATCH_SRC := config/linux_x86_64/dispatch.c
+CONFIG_DISPATCH_SRC := config/linux_avx2/dispatch.c
 CONFIG_TARGETS := avx2 reference
